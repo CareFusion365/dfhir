c9c4cdb4c34ede7a2ffdc1966cc95416f9fa945b
Elijah <elijahahianyo@gmail.com>
2025-04-09 10:06:32 +0000

    Initial unworking commit

---
+++ tests/organizations/test_model.py
@@ -0,0 +1,30 @@
+"""Test Organization model."""
+#
+# from django.contrib.auth import get_user_model
+# from django.test import TestCase
+#
+# from nebula.organizations.models import Organization
+#
+#
+# class TestOrganizationModel(TestCase):
+#     """Test Organization model."""
+#
+#     def test_create(self):
+#         """Test create organization."""
+#         organization = Organization.objects.create(
+#             name="Test Organization",
+#             website="http://test.com",
+#             email="example@mail.com",
+#             phone_number="+1234567890",
+#         )
+#         organization.admin = get_user_model().objects.create(
+#             email="admin@mail.com", username="admin", password="asdf"
+#         )
+#         organization.save()
+#
+#         assert Organization.objects.count() == 1
+#         assert Organization.objects.get().name == "Test Organization"
+#         assert Organization.objects.get().website == "http://test.com"
+#         assert Organization.objects.get().admin.email == "admin@mail.com"
+#         assert Organization.objects.get().admin.username == "admin"
+#         assert type(Organization.objects.get().pk) is int
---
+++ tests/organizations/test_views.py
@@ -0,0 +1,66 @@
+"""Test cases for the organizations views."""
+# TODO: need to fix tests
+# from django.test import TestCase
+# from rest_framework.test import APIRequestFactory
+#
+# from nebula.organizations.models import Organization
+# from nebula.organizations.views import OrganizationDetailView, OrganizationListView
+#
+#
+# class TestOrganizationListView(TestCase):
+#     """Test organization list view."""
+#
+#     def test_get(self):
+#         """Test get request."""
+#         factory = APIRequestFactory()
+#         request = factory.get("/api/organizations/")
+#         view = OrganizationListView.as_view()
+#         response = view(request)
+#         assert response.status_code == 200
+#
+#     def test_post(self):
+#         """Test post request."""
+#         factory = APIRequestFactory()
+#         request = factory.post("/api/organizations/", {"name": "Test Organization"})
+#         view = OrganizationListView.as_view()
+#         response = view(request)
+#         assert response.status_code == 200
+#         assert Organization.objects.count() == 1
+#         assert Organization.objects.get().name == "Test Organization"
+#
+#
+# class TestOrganizationDetailView(TestCase):
+#     """Test organization detail view."""
+#
+#     def test_get(self):
+#         """Test get request."""
+#         organization = Organization.objects.create(name="Test Organization")
+#         factory = APIRequestFactory()
+#         request = factory.get(f"/api/organizations/{organization.pk}/")
+#         view = OrganizationDetailView.as_view()
+#         response = view(request, pk=organization.pk)
+#         assert response.status_code == 200
+#         assert response.data["name"] == "Test Organization"
+#
+#     def test_patch(self):
+#         """Test patch request."""
+#         organization = Organization.objects.create(name="Test Organization")
+#         factory = APIRequestFactory()
+#         request = factory.patch(
+#             f"/api/organizations/{organization.pk}/", {"name": "Updated Organization"}
+#         )
+#         view = OrganizationDetailView.as_view()
+#         response = view(request, pk=organization.pk)
+#         assert response.status_code == 200
+#         assert Organization.objects.get().name == "Updated Organization"
+#
+#     def test_delete(self):
+#         """Test delete request."""
+#         organization = Organization.objects.create(name="Test Organization")
+#         factory = APIRequestFactory()
+#         request = factory.delete(f"/api/organizations/{organization.pk}/")
+#         view = OrganizationDetailView.as_view()
+#         response = view(request, pk=organization.pk)
+#         assert response.status_code == 204
+#         assert Organization.objects.count() == 0
+#         assert Organization.objects.exists() is False
