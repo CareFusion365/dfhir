b55260f7013a266593e515a986b8732f16d692dc
Ezra Yendau <5201040538@st.aamusted.edu.gh>
2025-04-16 12:50:51 +0000

    rename account.subject.reference.practitioner_role reference

--- dfhir/accounts/models.py
+++ dfhir/accounts/models.py
@@ -43,7 +43,7 @@ class AccountSubjectReference(BaseReference):
         null=True,
     )
     practitioner_role = models.ForeignKey(
-        "practitionerroles.PRactitionerRole",
+        "practitionerroles.PractitionerRole",
         related_name="account_subject_reference_practitioner_role",
         on_delete=models.CASCADE,
         null=True,
