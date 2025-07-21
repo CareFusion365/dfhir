e7397be650d88c1ee1a57264206cf1739328823f
Ezi-code <ezrayendau2000@hotmail.com>
2025-07-21 00:59:22 +0000

    remove unrelated files

--- tests/config/settings.py
+++ tests/config/settings.py
@@ -160,11 +160,7 @@ WSGI_APPLICATION = "demo1.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    "default": env.db(
-        "DATABASE_URL", default="postgres://postgres:admin@localhost:5432/dfhir"
-    )
-}
+DATABASES = {"default": env.db("DATABASE_URL")}
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 
 
