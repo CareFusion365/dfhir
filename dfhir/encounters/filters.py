8c37983e76c44d5b6eb0fe60880ed12cae3ed3a4
ezi-code <ezrayendau2000@hotmail.com>
2025-08-24 21:05:06 +0000

    update encounter filter file

--- dfhir/encounters/filters.py
+++ dfhir/encounters/filters.py
@@ -9,12 +9,18 @@ from .models import Encounter
 class EncounterFilter(filters.FilterSet):
     """Encounter filter."""
 
-    id = filters.CharFilter(field_name="id")
-    start_date_time = DateTimeFromToRangeFilter(field_name="start_date_time")
-    end_date_time = DateTimeFromToRangeFilter(field_name="end_date_time")
-    patient = filters.CharFilter(field_name="patient__id")
+    id = filters.CharFilter(field_name="id", lookup_expr="iexact")
+    start_date_time = DateTimeFromToRangeFilter(
+        field_name="start_date_time", lookup_expr="gte"
+    )
+    end_date_time = DateTimeFromToRangeFilter(
+        field_name="end_date_time", lookup_expr="lte"
+    )
+    patient = filters.CharFilter(
+        field_name="subject__actor__display", lookup_expr="icontains"
+    )
     practitioner = filters.CharFilter(
-        field_name="encounter_participant__practitioner__id"
+        field_name="encounter_participant__practitioner__id", lookup_expr="iexact"
     )
 
     class Meta:
