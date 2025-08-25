3930fdc3079e8a4f76d25df6d359c93b36e338b5
Ezi-code <ezrayendau2000@hotmail.com>
2025-08-25 00:13:58 +0000

    refactor filter

--- dfhir/appointments/filters.py
+++ dfhir/appointments/filters.py
@@ -9,17 +9,14 @@ from dfhir.appointments.models import Appointment
 class AppointmentFilter(filters.FilterSet):
     """appointment filter."""
 
-    id = filters.CharFilter(field_name="id", lookup_expr="iexact")
-    status = filters.CharFilter(field_name="statud", lookup_expr="icontains")
+    id = filters.CharFilter(field_name="id", lookup_expr="icontains")
+    status = filters.CharFilter(field_name="status", lookup_expr="icontains")
     start = DateFromToRangeFilter(field_name="start", lookup_expr="gte")
     end = DateFromToRangeFilter(field_name="end", lookup_expr="lte")
-    service_type = filters.CharFilter(
-        field_name="service_type", lookup_expr="icontains"
+    subject = filters.CharFilter(field_name="subject__display", lookup_expr="icontains")
+    participant = filters.CharFilter(
+        field_name="participant__actor__display", lookup_expr="icontains"
     )
-    reason = filters.CharFilter(field_name="reason", lookup_expr="icontains")
-    description = filters.CharFilter(field_name="description", lookup_expr="icontains")
-    subject = filters.CharFilter(field_name="subject", lookup_expr="icontains")
-    participant = filters.CharFilter(field_name="participant", lookup_expr="icontains")
 
     class Meta:
         """meta options."""
@@ -30,9 +27,6 @@ class AppointmentFilter(filters.FilterSet):
             "status",
             "start",
             "end",
-            "service_type",
-            "reason",
-            "description",
             "subject",
             "participant",
         ]
