1f1fc423494466e92cec5dd0a35bd2d370682d6e
ezi-code <ezrayendau2000@hotmail.com>
2025-09-04 22:22:16 +0000

    fix create organization test

--- tests/organizations/test_model.py
+++ tests/organizations/test_model.py
@@ -8,7 +8,13 @@ class TestOrganizationModel(OrganizationTestSetup):
 
     def test_create_organization(self):
         """Test create organization."""
-        assert self.serializer.validated_data["name"] == self.organization.name
-        assert self.serializer.validated_data["alias"] == self.organization.alias
-        assert self.serializer.validated_data["contact"] == self.organization.contact
-        assert self.serializer.validated_data["endpoint"] == self.organization.endpoint
+        assert self.serializer.data["name"] == self.organization.name
+        assert self.serializer.data["alias"] == self.organization.alias
+        assert self.serializer.data["website"] == self.organization.website
+        assert self.serializer.data["email"] == self.organization.email
+        assert self.serializer.data["status"] == self.organization.status
+        assert self.serializer.data["active"] == self.organization.active
+
+        assert self.serializer.data["part_of"] == self.organization.part_of
+        assert self.serializer.data["description"] == self.organization.description
+        assert self.serializer.data["id"] == self.organization.pk
