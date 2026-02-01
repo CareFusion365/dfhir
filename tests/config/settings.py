2f296a02d936506dbee595682fc0feeec495ee94
ezi-code <ezrayendau2000@hotmail.com>
2026-02-01 17:15:41 +0000

    run pre-commit

--- tests/config/settings.py
+++ tests/config/settings.py
@@ -9,8 +9,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
-from pathlib import Path
 import sys
+from pathlib import Path
 
 import environ
 
